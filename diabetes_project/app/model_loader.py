f27569308c27d828cd88589b99b8b0cd4954faf3
lokeswarimonapati <lokeswarimonapati16@gmail.com>
2026-02-17 00:56:41 -0800

    Delete diabetes_project directory

--- diabetes_project/app/model_loader.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import os
-
-MODEL_PATH = "model/diabetes_pipeline.pkl"
-
-def load_model():
-    if not os.path.exists(MODEL_PATH):
-        raise FileNotFoundError("Model file not found!")
-    return joblib.load(MODEL_PATH)
